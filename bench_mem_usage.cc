88dece378a5502463d9fc5078c45f6ed71cea1fd
mark <mmansi@utexas.edu>
2017-03-28 22:30:38 -0500

    Hmm... I thought I pushed this before...

--- bench_mem_usage.cc
+++ bench_mem_usage.cc
@@ -18,34 +18,17 @@ int main(int argc, char **argv)
 	size_t size = 1 << atoi(argv[1]);
 	size_t amt = 1 << atoi(argv[2]);
 
-	tmem = meminfo("MemTotal");
-	fmem = meminfo("MemFree");
-	pmem = statusinfo("VmRSS");
-	before = tmem - fmem - pmem;
-	printf("%lu\n", before);
+	printf("%lu %lu %lu\n", getkernelmem(), slabinfo("vm_area_struct"), meminfo("PageTables"));
 
-	for (i = 0; i < amt; i++) {
-		addr = (char *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-		if (addr == MAP_FAILED) {
-			fputs("mmap failed!\n", stderr);
-			return -1;
-		} else {
-	//		addr[0] = 'A';
-		}
+    for (i = 0; i < amt; i++) {
+        addr = (char *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+        if (addr == MAP_FAILED) {
+            fputs("mmap failed!\n", stderr);
+            return -1;
+        } else {
+            //		addr[0] = 'A';
+        }
 
-	tmem = meminfo("MemTotal");
-	fmem = meminfo("MemFree");
-	pmem = statusinfo("VmRSS");
-	after = tmem - fmem - pmem;
-	printf("%lu\n", after);
-
-	}
-
-	tmem = meminfo("MemTotal");
-	fmem = meminfo("MemFree");
-	pmem = statusinfo("VmRSS");
-	after = tmem - fmem - pmem;
-	printf("%lu\n", after);
-
-	return 0;
+        printf("%lu %lu %lu\n", getkernelmem(), slabinfo("vm_area_struct"), meminfo("PageTables"));
+    }
 }
