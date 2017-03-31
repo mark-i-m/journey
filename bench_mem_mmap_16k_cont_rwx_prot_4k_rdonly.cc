b649bfacbd5a0b0dad0f96a51ec8c9c40dc89088
mark <mmansi@utexas.edu>
2017-03-31 00:19:17 -0500

    Added no-touch version of protection fragmented test

---
+++ bench_mem_mmap_16k_cont_rwx_prot_4k_rdonly.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pattern.h"
+#include "rdmemusg.h"
+
+#include <sys/mman.h>
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("Usage: ./pattern number_order\n");
+        return -1;
+    }
+
+    size_t amt = 1 << atoi(argv[1]);
+
+    PatternGenerator p = get_frag_prot_pattern();
+    Next val;
+
+    printf("%lu\n", getkernelmem());
+
+    for (size_t i = 0; i < amt; i++) {
+        char *addr;
+        val = p.next();
+
+        if (val.operation == OP_MMAP) {
+            addr = (char *)mmap(val.address, val.size, val.permissions, MAP_ANON | MAP_PRIVATE, -1, 0);
+            if (addr == MAP_FAILED) {
+                fputs("mmap failed!\n", stderr);
+                return -1;
+            }
+        } else if (val.operation == OP_MPROT) {
+            int j = mprotect(val.address, val.size, val.permissions);
+            if (j) {
+                fputs("mprotect failed!\n", stderr);
+                return -1;
+            }
+        }
+
+        printf("%lu\n", getkernelmem());
+    }
+}
