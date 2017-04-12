d75c74a3b3fa862bb21ae22f8e0a52c177b8cf1a
Mark Mansi <markm@cs.wisc.edu>
2017-04-12 18:08:11 -0500

    Check mlock failure

--- bench_mmap_anon_rss.cc
+++ bench_mmap_anon_rss.cc
@@ -13,38 +13,35 @@
 int main(int argc, char **argv) {
     set_cpu(0);
 
-
-	if (argc < 1) {
-        std::cout << "Usage: ./memusage number_order" << std::endl;
-        // whatever!
-		return -1;
-	}
-
     uint64_t vma_size = (uint64_t)1 << 35; // fixed at 32 GB
 
-    char* start_addr = (char*)mmap(0, vma_size, 7, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    char* start_addr = (char*)mmap(NULL, vma_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
     if (start_addr == MAP_FAILED) {
-       printf("fuck\n");
-       exit(1);
+        std::cerr << "mmap failed: " << strerror(errno) << std::endl;
+        exit(1);
     }
 
     unsigned long long num_pages_mapped = vma_size / PAGE_SIZE;
     unsigned long* res_array = (unsigned long*)malloc(num_pages_mapped * 
             sizeof(unsigned long));
-    // lock this results array itself so itself is not swapped out
-    mlock((const void*) res_array, num_pages_mapped * sizeof(unsigned long));
 
+    // lock this results array itself so itself is not swapped out
+    if(!mlock((const void*) res_array, num_pages_mapped * sizeof(unsigned long))) {
+        std::cerr << "mlock failed: " << strerror(errno) << std::endl;
+        exit(1);
+    }
     
     // touch all the pages now
     unsigned long long num_touched = 0;
     char* addr = start_addr;
     while (num_touched < num_pages_mapped) {
-        addr[0] = 'X';
+        *addr = 'X';
         res_array[num_touched++] = statusinfo("VmRSS");
 
         addr += PAGE_SIZE;
     }
 
-    for (unsigned int i = 0; i < num_pages_mapped; i++) std::cout << res_array[i++] << std::endl;
-
+    for (unsigned int i = 0; i < num_pages_mapped; i++) {
+        std::cout << res_array[i++] << std::endl;
+    }
 }
